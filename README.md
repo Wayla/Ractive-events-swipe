005b6ef1e09d95e54a94b21a0ab017193cac4641
Brett Shwom <brett.shwom@nyu.edu>
2014-01-15 23:14:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Ractive.js swipe event plugin
+===========================
+
+A `swipe` event is fired when a user swipes her finger across the screen.
+
+*Find more Ractive.js plugins at [ractivejs.org/plugins](http://ractivejs.org/plugins)*
+
+Usage
+-----
+
+Include this file on your page below Ractive, e.g:
+
+```html
+<script src='lib/Ractive.js'></script>
+<script src='lib/Ractive-events-swipe.js'></script>
+```
+
+Or, if you're using a module loader, require this module:
+
+```js
+// requiring the plugin will 'activate' it - no need to use the return value
+require( 'Ractive-events-swipe' );
+```
+
+
---
+++ Ractive-events-swipe.js
@@ -0,0 +1,47 @@
+//code copied and modified from https://github.com/RactiveJS/Ractive-events-tap/blob/18aec5fae805f3e160f19408b7866fd459b6cd0c/Ractive-events-tap.js
+
+(function ( global, factory ) {
+
+  'use strict';
+
+  // Common JS (i.e. browserify) environment
+  if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
+    factory( require( 'Ractive' ), require( 'hammerjs' ) );
+  }
+
+  // AMD?
+  else if ( typeof define === 'function' && define.amd ) {
+    define([ 'Ractive', 'hammerjs' ], factory );
+  }
+
+  // browser global
+  else if ( global.Ractive && global.Hammer) {
+    factory( global.Ractive, global.Hammer );
+  }
+
+  else {
+    throw new Error( 'Could not find Ractive! It must be loaded before the long-tap plugin' );
+  }
+
+
+}( typeof window !== 'undefined' ? window : this, function ( Ractive, Hammer ) {
+
+  'use strict';
+
+  var swipe = function ( node, fire ) {
+
+    Hammer(node).on("swipe", function(event) {
+      fire({
+        node: node,
+        original: event //actually a hammerjs event -- to get at the original event use gesture.srcEvent
+      })
+    });
+
+    return {
+      teardown: function () {} //probably need to define this at some point
+    };
+  };
+
+  Ractive.events.swipe = swipe;
+
+}));
